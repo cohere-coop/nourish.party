917e3f366fd399dacb7db1ab1eb830b5feabbe6e
Zee Spencer <zee@wecohere.com>
2017-12-17 11:24:00 -0800

    Users may be instance admins
    As we build out more functionality, being able to expose functionality
    based upon whether someone is an instance admin will be useful.
    
    This brings in the ability to be set as an admin via a boolean, we may
    eventually need to do fancy things like group ownerships and different
    levels or kinds of admins, but that seems like something we should be
    able to evolve towards without too much difficulty

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the rails db:seed command (or created alongside the database with
+# db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+raise "Probably shouldn't run this on production..." if Rails.env.production?
+
+User.create_with(password: "password", instance_admin: true)
+    .find_or_create_by(email: "admin@example.com")

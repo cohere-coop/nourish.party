927781e13855f3eb5cd2bb9b6b6f4c30d05fa11d
Betsy Haibel <betsy.haibel@gmail.com>
2017-12-23 17:28:22 +0000

    Add CSS scaffolding (Bourbon/Bitters) (#53)
    * Use Bourbon for SCSS utilities
    * Use Bitters for as a normalize/style scaffold

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
+const _ = require('lodash')
+const eyeglass = require('eyeglass')
+
+// add eyeglass to sass-loader config
+const sassLoaderChain = environment.loaders.get('sass')
+const baseSassLoaderConfig = _.find(sassLoaderChain.use, config => config.loader === 'sass-loader')
+baseSassLoaderConfig.options = eyeglass({
+  ...baseSassLoaderConfig.options,
+  eyeglass: {}
+})
 
 module.exports = environment

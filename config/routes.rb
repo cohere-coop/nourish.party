b92f067879d456f8808d3776b06f9cba6f08b22c
Zee <zee@wecohere.com>
2017-12-25 14:51:44 -0800

    Set up codeclimate for static analysis (#62)
    * Bring styles in line with codeclimates rubocop version
    * Enable coverage reporting for app/* files

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "dashboard#index"
 
-  resources :projects, only: [:new, :create]
+  resources :projects, only: %i[new create]
 end
---
+++ spec/support/simplecov.rb
@@ -0,0 +1,7 @@
+require "coveralls"
+require "simplecov"
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start("rails")

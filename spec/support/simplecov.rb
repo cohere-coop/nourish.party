c5e1435b7f611716cc69b6c984625d190e7587d4
Zee <zee@wecohere.com>
2017-12-28 10:08:22 -0800

    Include Cucumber coverage in Coveralls (#66)
    * Merge test results
    
    * Include cucumber with feature test coverage

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -1,7 +1,5 @@
 require "coveralls"
 require "simplecov"
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::HTMLFormatter,
+                                                                Coveralls::SimpleCov::Formatter])
 SimpleCov.start("rails")

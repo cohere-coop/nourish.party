9a7e2b55d7a56f13be5e4169bd1b75ce307df990
Betsy Haibel <betsy.haibel@gmail.com>
2017-12-28 12:50:33 -0500

    KSS runs

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,3 +1,8 @@
 const environment = require('./environment')
+const KssWebpackPlugin = require('kss-webpack-plugin')
+
+environment.plugins.prepend('Kss', new KssWebpackPlugin({
+  source: 'app/assets/src/stylesheets'
+}))
 
 module.exports = environment.toWebpackConfig()

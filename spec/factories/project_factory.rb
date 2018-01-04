516e8f2997345609316c96839442f7beaa6e6941
Zee <zee@wecohere.com>
2018-01-04 15:19:07 -0800

    Allow multiple users and projects to be created in tests factories (#103)
    Turns out we had an accidental limitation where we weren't generating unique data when we thought we were in our factories. Oops!

--- spec/factories/project_factory.rb
+++ spec/factories/project_factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :project do
-    title "Title #{SecureRandom.uuid}"
-    summary "Summary #{SecureRandom.uuid}"
+    title { "Title #{SecureRandom.uuid}" }
+    summary { "Summary #{SecureRandom.uuid}" }
   end
 end

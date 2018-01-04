cbfe8732cf0f80b25aa8f8d079ffeed9fff9ed93
Zee Spencer <zee@wecohere.com>
2018-01-04 15:10:43 -0800

    Fix factories so multiple users may be created

--- spec/factories/registered_user_factory.rb
+++ spec/factories/registered_user_factory.rb
@@ -1,13 +1,11 @@
 FactoryBot.define do
-  factory :registered_user do
-    email "user-#{SecureRandom.uuid}@example.com"
+  factory :registered_user, aliases: [:user] do
+    email { "user-#{SecureRandom.uuid}@example.com" }
     password "password"
 
     factory :instance_admin do
+      email { "instance-admin-#{SecureRandom.uuid}@example.com" }
       instance_admin true
-    end
-
-    factory :user do
     end
   end
 end

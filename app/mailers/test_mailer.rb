951f0423171d3362ce2fa79e5ad462df0c22806c
Zee Spencer <zee@wecohere.com>
2018-01-04 11:42:45 -0800

    Remove test mailer now that it works

--- app/mailers/test_mailer.rb
+++
@@ -1,6 +0,0 @@
-# Mailer used to test that emails actually go out
-class TestMailer < ApplicationMailer
-  def test_email(address:)
-    mail(to: address, subject: "Nourish can send emails!")
-  end
-end

9c727d0b8dcd8b9a0ba6b840f03adf37654e3f87
Zee Spencer <zee@wecohere.com>
2018-01-07 11:13:28 -0800

    Refactor contribution amounts to feel more like money-rails

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -4,13 +4,14 @@ class Contribution
 
   attr_reader :amount_in_dollars
   attr_writer :payment_processor
+  attr_accessor :amount_cents
 
   def create
     payment_processor.charge(amount: amount)
   end
 
   def amount_in_dollars=(amount_in_dollars)
-    (@amount_in_dollars = amount_in_dollars.to_f) && return if amount_in_dollars.respond_to?(:to_f)
+    (self.amount_cents = amount_in_dollars.to_f * 100) && return if amount_in_dollars.respond_to?(:to_f)
     raise ArgumentError, "amount_in_dollars must respond to #to_f, but is a #{amount_in_dollars.class}"
   end
 
@@ -21,7 +22,7 @@ class Contribution
   end
 
   private def amount
-    Money.new(amount_in_dollars * 100, "USD")
+    Money.new(amount_cents, "USD")
   end
 
   def payment_processor

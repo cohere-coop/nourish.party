146a2167582b97764e245d2a3394ad166419df27
Zee Spencer <zee@wecohere.com>
2018-01-07 19:52:13 -0800

    Refactor to use the self << class syntax

--- app/lib/nourish/stripe.rb
+++ app/lib/nourish/stripe.rb
@@ -1,16 +1,15 @@
 module Nourish
   # Nourish Stripe plugin for accepting payments via stripe
   class Stripe
-    def self.instance
-      @instance ||= new
+    def create_charge(*args, **kwargs)
+      ::Stripe::Charge.create(*args, **kwargs)
     end
 
     class << self
       attr_writer :instance
-    end
-
-    def create_charge(*args, **kwargs)
-      ::Stripe::Charge.create(*args, **kwargs)
+      def instance
+        @instance ||= new
+      end
     end
   end
 end
